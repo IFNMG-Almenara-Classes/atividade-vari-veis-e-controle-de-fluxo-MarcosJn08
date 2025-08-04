697bb54ac69063bad6da2b39f84b94c24e18220e
Marcos J <mjrs2207@gmail.com>
2025-08-03 22:58:40 -0300

    questao11

---
+++ src/questao11/Principal.java
@@ -0,0 +1,18 @@
+package questao11;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int valor = scanner.nextInt();
+
+        for(int i = 0;i<=valor;i++){
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
