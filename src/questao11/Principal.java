6f362e782e630eabf2d1ca5622fb9428c51be971
Marcos J <mjrs2207@gmail.com>
2025-08-03 23:00:31 -0300

    correção de erro

--- src/questao11/Principal.java
+++ src/questao11/Principal.java
@@ -3,7 +3,7 @@ package questao11;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String args) {
+    public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite um numero: ");
