01f278c1da5592538c18b71b0b67f4840b940102
Marcos J <mjrs2207@gmail.com>
2025-08-03 23:46:57 -0300

    Questao 13 part 2

--- src/questao13/Principal.java
+++ src/questao13/Principal.java
@@ -1,57 +1,76 @@
 package questao13;
+
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int escolha;
+        int escolha1;
+        int escolha2;
         int valor1;
         int valor2;
 
-        do{
+        do {
             System.out.println("1 - Somar \n2 - Subtrair \n3 - Multiplicar \n4 - Dividir \n5 - Sair");
-            escolha = scanner.nextInt();
+            escolha1 = scanner.nextInt();
 
-            if(escolha == 1){
+            if (escolha1 == 1) {
                 System.out.println("Digite o primeiro valor: ");
                 valor1 = scanner.nextInt();
 
                 System.out.println("Digite o segundo valor: ");
                 valor2 = scanner.nextInt();
 
-                System.out.println("Resultado:: "+(valor1+valor2));
-            }
-            else if(escolha == 2){
+                System.out.println("Resultado:: " + (valor1 + valor2));
+            } else if (escolha1 == 2) {
                 System.out.println("Digite o primeiro valor: ");
                 valor1 = scanner.nextInt();
 
                 System.out.println("Digite o segundo valor: ");
                 valor2 = scanner.nextInt();
 
-                System.out.println("Resultado:: "+(valor1-valor2));
-            }
-            else if(escolha == 3){
+                System.out.println("Opções: \n1-primeiro valor - segundo valor \n2-segundo valor - primeiro valor");
+                escolha2 = scanner.nextInt();
+
+                if (escolha2 == 1) {
+                    System.out.println("Resultado:: " + (valor1 - valor2));
+                } else if (escolha2 == 2) {
+                    System.out.println("Resultado:: " + (valor2 - valor1));
+                } else {
+                    System.out.println("Opção invalidade, tente novamente!");
+                }
+
+            } else if (escolha1 == 3) {
                 System.out.println("Digite o primeiro valor: ");
                 valor1 = scanner.nextInt();
 
                 System.out.println("Digite o segundo valor: ");
                 valor2 = scanner.nextInt();
 
-                System.out.println("Resultado: "+(valor1*valor2));
-            }
-            else if(escolha == 4){
+                System.out.println("Resultado: " + (valor1 * valor2));
+            } else if (escolha1 == 4) {
                 System.out.println("Digite o primeiro valor: ");
                 valor1 = scanner.nextInt();
 
                 System.out.println("Digite o segundo valor: ");
                 valor2 = scanner.nextInt();
 
-                System.out.println("Resultado: "+(valor1/valor2));
-            }
-            else if(escolha >=6){
+                System.out.println("Opções: \n1-primeiro valor / segundo valor \n2-segundo valor / primeiro valor");
+                escolha2 = scanner.nextInt();
+
+                if (escolha2 == 1) {
+                    System.out.println("Resultado:: " + (valor1 / valor2));
+                } else if (escolha2 == 2) {
+                    System.out.println("Resultado:: " + (valor2 / valor1));
+                } else {
+                    System.out.println("Opção invalidade, tente novamente!");
+                }
+            } else if (escolha1 >= 6) {
                 System.out.println("Esse valor não existe, tente novamente");
             }
-        }while(escolha != 5);
+        } while (escolha1 != 5);
+
+        System.out.println("Calculadora finalizada com Sucesso!");
     }
 }
